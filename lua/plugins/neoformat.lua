3480c1bc8df56aa99ce1e4d4b6479f4ab690563a
Jamie Macdonald <jamie.alban@gmail.com>
2023-09-03 14:39:29 -0700

    Add css and html to prettierd formatter

--- lua/plugins/neoformat.lua
+++ lua/plugins/neoformat.lua
@@ -1,14 +1,14 @@
 return {
-    'sbdchd/neoformat',
-    config = function()
-        local augroup = vim.api.nvim_create_augroup("fmt", { clear = true })
-        vim.api.nvim_create_autocmd({ "BufWritePre" },
-            {
-                group = augroup,
-                pattern = { "*.js", "*.jsx", "*.ts", "*.tsx" },
-                callback = function()
-                    vim.cmd("Neoformat prettierd")
-                end
-            })
-    end,
+  'sbdchd/neoformat',
+  config = function()
+    local augroup = vim.api.nvim_create_augroup("fmt", { clear = true })
+    vim.api.nvim_create_autocmd({ "BufWritePre" },
+      {
+        group = augroup,
+        pattern = { "*.js", "*.jsx", "*.ts", "*.tsx", "*.css", "*.html" },
+        callback = function()
+          vim.cmd("Neoformat prettierd")
+        end
+      })
+  end,
 }
