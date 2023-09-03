31128ba3ef47ee467a32b5fad637f407803373a3
Jamie Macdonald <jamie.alban@gmail.com>
2023-09-03 15:27:50 -0700

    Add bufferline and keymaps

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -1,5 +1,20 @@
 return {
-    'akinsho/bufferline.nvim',
-    version = "*",
-    dependencies = 'nvim-tree/nvim-web-devicons'
+  'akinsho/bufferline.nvim',
+  version = "*",
+  dependencies = 'nvim-tree/nvim-web-devicons',
+  config = function()
+    local opts = {
+      options = {
+        diagnostics = "nvim_lsp",
+        groups = {
+          items = {
+            require('bufferline.groups').builtin.pinned:with({ icon = "📌" })
+          }
+        }
+      }
+    }
+    require('bufferline').setup(opts)
+
+    vim.keymap.set({ 'n' }, '<leader>p', function() vim.cmd('BufferLineTogglePin') end, { desc = '[P]in buffer' })
+  end,
 }
