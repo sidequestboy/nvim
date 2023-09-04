5e32274e217ca582b41d2a9cd7facbb32620eeae
Jamie Macdonald <jamie.alban@gmail.com>
2023-09-04 00:54:13 -0700

    Tweak bufferline

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -4,16 +4,32 @@ return {
   dependencies = 'nvim-tree/nvim-web-devicons',
   after = "catppuccin",
   config = function()
+    local error_sign = vim.fn.sign_getdefined("DiagnosticSignError")[1].text or "󰅚"
+    local warning_sign = vim.fn.sign_getdefined("DiagnosticSignWarn")[1].text or "󰀪"
+
     local opts = {
       options = {
+        sort_by = "insert_after_current",
+        separator_style = "slant",
         diagnostics = "nvim_lsp",
+        diagnostics_indicator = function(count, level, diagnostics_dict, context)
+          if (level:match("error")) then
+            return error_sign
+          elseif (level:match("warning")) then
+            return warning_sign
+          else
+            return ""
+          end
+        end,
         groups = {
           items = {
             require('bufferline.groups').builtin.pinned:with({ icon = "📌" })
           }
         }
       },
-      highlights = require("catppuccin.groups.integrations.bufferline").get()
+      highlights = require("catppuccin.groups.integrations.bufferline").get({
+        styles = { "italic", "bold" },
+      })
     }
     require('bufferline').setup(opts)
 
