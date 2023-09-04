4423aa9908d1e53534b476a786a9acb71152ca24
Jamie Macdonald <jamie.alban@gmail.com>
2023-09-04 15:59:38 -0700

    Re-enable gitsigns

--- lua/plugins/misc.lua
+++ lua/plugins/misc.lua
@@ -19,7 +19,6 @@ return {
   {
     -- Adds git related signs to the gutter, as well as utilities for managing changes
     'lewis6991/gitsigns.nvim',
-    enabled = false,
     opts = {
       -- See `:help gitsigns.txt`
       signs = {
