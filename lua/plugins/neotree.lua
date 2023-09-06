5839bba47ab7321fc7b731d4568783e1cc631dbf
Jamie Macdonald <jamie.alban@gmail.com>
2023-09-06 03:06:23 -0700

    Lazy load neotree on keybind

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -13,16 +13,25 @@ return {
     enable_diagnostics = true,
     sources = { "filesystem", "document_symbols" },
   },
-  config = function(_, opts)
-    require('neo-tree').setup(opts)
-    vim.keymap.set('n', '<leader>dS',
-      function() vim.cmd("Neotree source=document_symbols reveal=true position=right toggle=true") end,
-      { desc = '[n]eotree toggle [d]ocument [S]ymbols' })
-    vim.keymap.set('n', '<leader>f',
-      function() vim.cmd("Neotree source=filesystem reveal=true position=left toggle=true") end,
-      { desc = 'Neotree toggle filesystem' })
-    vim.keymap.set('n', '<leader>t',
+  keys = {
+    {
+      '<leader>dS',
+      function()
+        vim.cmd("Neotree source=document_symbols reveal=true position=right toggle=true")
+      end,
+      desc = '[n]eotree toggle [d]ocument [S]ymbols',
+    },
+    {
+      '<leader>f',
+      function()
+        vim.cmd("Neotree source=filesystem reveal=true position=left toggle=true")
+      end,
+      desc = 'Neotree toggle filesystem',
+    },
+    {
+      '<leader>t',
       function() vim.cmd("Neotree position=left toggle=true") end,
-      { desc = 'Neo[t]ree toggle' })
-  end,
+      desc = 'Neo[t]ree toggle',
+    },
+  }
 }
