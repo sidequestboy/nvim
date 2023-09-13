c5b5adbac480f6f7014a5894b4fdc2249eb5623c
Jamie Macdonald <jamie.alban@gmail.com>
2023-09-13 01:40:42 -0700

    Increase frame rate

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -21,6 +21,7 @@ return {
       inc_rename = false, -- enables an input dialog for inc-rename.nvim
       lsp_doc_border = false, -- add a border to hover docs and signature help
     },
+    throttle = 1000 / 120,
   },
   dependencies = {
     -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
