08b6969e4b8f40c7b5f70e14c1df2d6449b56e1d
Jamie Macdonald <jamie.alban@gmail.com>
2023-09-13 00:20:41 -0700

    Add autoformat user command

--- lua/plugins/neoformat.lua
+++ lua/plugins/neoformat.lua
@@ -1,19 +1,40 @@
 return {
   'sbdchd/neoformat',
   config = function()
+    local auto_format_enabled = true
+    vim.api.nvim_create_user_command("AutoFormatEnable", function()
+      auto_format_enabled = true
+      print("Auto format enabled.")
+    end, {})
+    vim.api.nvim_create_user_command("AutoFormatDisable", function()
+      auto_format_enabled = false
+      print("Auto format disabled.")
+    end, {})
+    vim.api.nvim_create_user_command("AutoFormatToggle", function()
+      if auto_format_enabled then
+        vim.cmd("AutoFormatDisable")
+      else
+        vim.cmd("AutoFormatEnable")
+      end
+    end, {})
+
     local augroup = vim.api.nvim_create_augroup('fmt', { clear = true })
     vim.api.nvim_create_autocmd({ 'BufWritePre' }, {
       group = augroup,
       pattern = { '*.js', '*.jsx', '*.ts', '*.tsx', '*.css', '*.html' },
       callback = function()
-        vim.cmd 'Neoformat prettierd'
+        if auto_format_enabled then
+          vim.cmd 'Neoformat prettierd'
+        end
       end,
     })
     vim.api.nvim_create_autocmd({ 'BufWritePre' }, {
       group = augroup,
       pattern = { '*.lua' },
       callback = function()
-        vim.cmd 'Neoformat stylua'
+        if auto_format_enabled then
+          vim.cmd 'Neoformat stylua'
+        end
       end,
     })
   end,
