d26f8c743ff7b88362acefb0794ac24b66287927
Jamie Macdonald <jamie.alban@gmail.com>
2023-09-16 20:43:04 -0700

    Add markdown preview

--- lua/plugins/misc.lua
+++ lua/plugins/misc.lua
@@ -1,5 +1,12 @@
 return {
-
+  {
+    'iamcco/markdown-preview.nvim',
+    ft = 'markdown',
+    build = 'cd app && npm install',
+    init = function()
+      vim.g.mkdp_filetypes = { 'markdown' }
+    end,
+  },
   {
     'glepnir/nerdicons.nvim',
     cmd = 'NerdIcons',
