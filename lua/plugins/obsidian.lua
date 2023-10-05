369ef83515d1f93cb8aa61fcd854e045be359b97
Jamie Macdonald <jamie.alban@gmail.com>
2023-10-04 23:06:10 -0700

    Use local obsidian plugin

--- lua/plugins/obsidian.lua
+++ lua/plugins/obsidian.lua
@@ -1,35 +1,29 @@
-local vault_path = vim.fn.expand '~' .. '/my/docs/sidequestboy'
+local vault_path = vim.fn.expand '~/my/docs/sidequestboy'
 return {
-  'epwalsh/obsidian.nvim',
-  lazy = false,
-  -- event = {
-  --   "BufReadPre " .. vault_path .. "/**.md",
-  --   "BufNewFile " .. vault_path .. "/**.md",
-  -- },
-  dependencies = {
-    'nvim-lua/plenary.nvim',
-  },
-  opts = {
-    dir = vault_path,
-    mappings = {},
-  },
-  keys = {
-    {
-      '<leader>so',
-      function()
-        vim.cmd 'ObsidianQuickSwitch'
-      end,
-      desc = '[S]earch [O]bsidian',
+  {
+    'epwalsh/obsidian.nvim',
+    lazy = true,
+    tag = 'v1.13.0',
+    event = {
+      'BufReadPre ' .. vault_path .. '/**.md',
+      'BufNewFile ' .. vault_path .. '/**.md',
+    },
+    dependencies = {
+      -- Required.
+      'nvim-lua/plenary.nvim',
+    },
+    opts = {
+      dir = '~/my/docs/sidequestboy', -- no need to call 'vim.fn.expand' here
     },
   },
-  config = function(_, opts)
-    require('obsidian').setup(opts)
-    vim.keymap.set('n', 'gf', function()
-      if require('obsidian').util.cursor_on_markdown_link() then
-        return ':ObsidianFollowLink<cr>'
-      else
-        return 'gf'
-      end
-    end, { noremap = false, expr = true })
-  end,
+  {
+    'oflisback/obsidian-sync.nvim',
+    dev = true,
+    dir = '~/my/code/nvim-plugins/obsidian-sync.nvim',
+    branch = 'topic/apply-uriencode',
+    opts = {
+      vault_path = vault_path,
+      obsidian_server_address = 'http://localhost:27123',
+    },
+  },
 }
