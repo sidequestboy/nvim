a858f26f1d05037696b13359aa61f333b3147816
Jamie Macdonald <jamie.alban@gmail.com>
2023-10-05 01:14:07 -0700

    update indent blankline

--- lua/plugins/misc.lua
+++ lua/plugins/misc.lua
@@ -114,9 +114,15 @@ return {
     'lukas-reineke/indent-blankline.nvim',
     -- Enable `lukas-reineke/indent-blankline.nvim`
     -- See `:help indent_blankline.txt`
+    main = 'ibl',
     opts = {
-      char = '┊',
-      show_trailing_blankline_indent = false,
+      indent = {
+        char = '┊',
+      },
+      scope = {
+        char = '┊',
+        show_start = false,
+      },
     },
   },
 
