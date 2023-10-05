fcc159cf6d4f14f3267de01afa8f06654fd978fa
Jamie Macdonald <jamie.alban@gmail.com>
2023-10-04 23:06:46 -0700

    formatting

--- lua/plugins/neoformat.lua
+++ lua/plugins/neoformat.lua
@@ -2,30 +2,40 @@ return {
   'sbdchd/neoformat',
   config = function()
     local auto_format_enabled = true
-    vim.api.nvim_create_user_command("AutoFormatEnable", function()
+    vim.api.nvim_create_user_command('AutoFormatEnable', function()
       auto_format_enabled = true
-      print("Auto format enabled.")
+      print 'Auto format enabled.'
     end, {})
-    vim.api.nvim_create_user_command("AutoFormatDisable", function()
+    vim.api.nvim_create_user_command('AutoFormatDisable', function()
       auto_format_enabled = false
-      print("Auto format disabled.")
+      print 'Auto format disabled.'
     end, {})
-    vim.api.nvim_create_user_command("AutoFormatToggle", function()
+    vim.api.nvim_create_user_command('AutoFormatToggle', function()
       if auto_format_enabled then
-        vim.cmd("AutoFormatDisable")
+        vim.cmd 'AutoFormatDisable'
       else
-        vim.cmd("AutoFormatEnable")
+        vim.cmd 'AutoFormatEnable'
       end
     end, {})
 
     local augroup = vim.api.nvim_create_augroup('fmt', { clear = true })
+    local prettierd_filetypes = { '*.js', '*.jsx', '*.ts', '*.tsx', '*.css', '*.scss', '*.html' }
     vim.api.nvim_create_autocmd({ 'BufWritePre' }, {
       group = augroup,
-      pattern = { '*.js', '*.jsx', '*.ts', '*.tsx', '*.css', '*.html' },
+      pattern = prettierd_filetypes,
       callback = function()
         if auto_format_enabled then
           vim.cmd 'Neoformat prettierd'
         end
+      end,
+    })
+    vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWinEnter' }, {
+      group = augroup,
+      pattern = prettierd_filetypes,
+      callback = function()
+        vim.keymap.set({ 'n' }, '<leader>=', function()
+          vim.cmd 'Neoformat prettierd'
+        end, { desc = 'Neoformat', buffer = true })
       end,
     })
     vim.api.nvim_create_autocmd({ 'BufWritePre' }, {
@@ -35,6 +45,15 @@ return {
         if auto_format_enabled then
           vim.cmd 'Neoformat stylua'
         end
+      end,
+    })
+    vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWinEnter' }, {
+      group = augroup,
+      pattern = { '*.lua' },
+      callback = function()
+        vim.keymap.set({ 'n' }, '<leader>=', function()
+          vim.cmd 'Neoformat stylua'
+        end, { desc = 'Neoformat', buffer = true })
       end,
     })
   end,
